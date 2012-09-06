d63699970c4605642d80fef3ac9c5df39f56c7bf
RJ Zaworski <rj@rjzaworski.com>
2012-09-06 11:37:50 -0700

    moved mock geocoder to helper

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,45 @@
 (function () {
-    this.testHelper = {
 
+    // provide mock geocoder service
+    (function (service) {
+
+        service.geocode = function (originalLocation, opts) {
+            var response = opts.response || {};
+            service.trigger(response.type, originalLocation);
+            if (opts[response.type] instanceof Function) {
+                opts[response.type](originalLocation);
+            }
+        };
+
+    })(geous.geocoders('mock'));
+
+    // provide mock navigator.location object
+    geous.extend(navigator.geolocation, {
+
+        nextResult: function (success, lat, lng) {
+            this._nextResult = [success, lat, lng];
+        },
+
+        getCurrentPosition: function (success, error) {
+
+            var result;
+
+            if ((result = this._nextResult) && result[0]) {
+                return success({
+                    coords: {
+                        latitude: result[1],
+                        longitude: result[2]
+                    }
+                });
+            } else {
+                return error();
+            }
+        }
+    });
+
+
+    // include test helpers
+    this.testHelper = {
         checkFields: function (location, fields) {
             var scope;
 
